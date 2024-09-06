aeae6fb8dcf4f4edc95dd76cec556b1625d5f59d
Pedro Miguel <109111199+PedroMiguel33@users.noreply.github.com>
2024-09-05 21:15:31 -0300

    Add files via upload

---
+++ trilha-net-fundamentos-desafio/DesafioFundamentos/Models/Estacionamento.cs
@@ -0,0 +1,58 @@
+namespace DesafioFundamentos.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.Write("Digite a placa do veículo para estacionar: ");
+            veiculos.Add(Console.ReadLine());
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.Write("Digite a placa do veículo para remover: ");
+            string placa = Console.ReadLine();
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.Write("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                int horas = int.Parse(Console.ReadLine());      
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+                veiculos.Remove(placa);
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            // Verifica se há veículos no estacionamento
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                int x = 1; // COLOQUEI INICIANDO COM 1 PARA DEIXAR VISIVELMENTE MAIS BONITO NO CONSOLE.
+                foreach (var item in veiculos)
+                {
+                    Console.WriteLine("{0}° - {1}", x, item);
+                    x++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
